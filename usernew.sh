4e8e9d56afa34c65239d3e35d770764d56e34b34
Xssgit47 <xarteastmin@gmail.com>
2024-11-17 22:57:58 +0530

    Update usernew.sh

--- usernew.sh
+++ usernew.sh
@@ -43,8 +43,8 @@ export WARNING="${RED}\e[5m"
 export UNDERLINE="\e[4m"
 
 # // Exporting URL Host
-export Server_URL="raw.githubusercontent.com/NevermoreSSH/Blueblue/main/test"
-export Server1_URL="raw.githubusercontent.com/NevermoreSSH/Blueblue/main/limit"
+export Server_URL="raw.githubusercontent.com/Xssgit47/Blueblue/main/test"
+export Server1_URL="raw.githubusercontent.com/Xssgit47/Blueblue/main/limit"
 export Server_Port="443"
 export Server_IP="underfined"
 export Script_Mode="Stable"
@@ -110,7 +110,7 @@ cat > /home/vps/public_html/ssh-$Login.txt <<-END
              P R O J E C T  O F  N E V E R M O R E S S H
                        [Freedom Internet]
 ====================================================================
-            https://github.com/NevermoreSSH
+            https://github.com/Xssgit47
 ====================================================================
               Format SSH OVPN Account
 ====================================================================
