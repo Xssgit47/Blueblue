fbb2af8fa133147b6cf4437be6f14cbd776f4462
Xssgit47 <xarteastmin@gmail.com>
2024-11-17 22:43:09 +0530

    Update menu4.sh

--- menu4.sh
+++ menu4.sh
@@ -57,8 +57,8 @@ export WARNING="${RED}\e[5m"
 export UNDERLINE="\e[4m"
 
 # // Exporting URL Host
-export Server_URL="raw.githubusercontent.com/NevermoreSSH/Blueblue/main/test"
-export Server1_URL="raw.githubusercontent.com/NevermoreSSH/Blueblue/main/limit"
+export Server_URL="raw.githubusercontent.com/Xssgit47/Blueblue/main/test"
+export Server1_URL="raw.githubusercontent.com/Xssgit47/Blueblue/main/limit"
 export Server_Port="443"
 export Server_IP="underfined"
 export Script_Mode="Stable"
@@ -294,9 +294,9 @@ case $opt in
 23) clear ; dns ;;
 24) clear ; netf ;;
 25) clear ; tendang ;;
-55) clear ; wget -q -O /usr/bin/xraychanger "https://raw.githubusercontent.com/NevermoreSSH/Xcore-custompath/main/xraychanger.sh" && chmod +x /usr/bin/xraychanger && xraychanger ;;
+55) clear ; wget -q -O /usr/bin/xraychanger "https://raw.githubusercontent.com/Xssgit47/Xcore-custompath/main/xraychanger.sh" && chmod +x /usr/bin/xraychanger && xraychanger ;;
 66) clear ; bbr ;;
-77) clear ; wget -q -O /usr/bin/swapram "https://raw.githubusercontent.com/NevermoreSSH/swapram/main/swapram.sh" && chmod +x /usr/bin/swapram && swapram ;;
+77) clear ; wget -q -O /usr/bin/swapram "https://raw.githubusercontent.com/Xssgit47/swapram/main/swapram.sh" && chmod +x /usr/bin/swapram && swapram ;;
 88) clear ; backup ;;
 99) clear ; restore ;;
 #88) clear ; wget https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/addons/dns2.sh && chmod +x dns2.sh && ./dns2.sh ;;
